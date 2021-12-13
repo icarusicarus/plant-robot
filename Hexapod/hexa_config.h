a2af04193b66970ce51b9cf987d0e6e36e7e5599
Yeryeong Kim <76420409+icarusicarus@users.noreply.github.com>
2021-12-13 17:56:38 +0900

    Add configs

--- Hexapod/hexa_config.h
+++ Hexapod/hexa_config.h
@@ -1,4 +1,49 @@
-// Start Position
-#define HexInitPosF 150
-#define HexInitPosS 115
-#define HexInitPosT 110
+// Start Angles
+#define InitPosCoxa 150
+#define InitPosFemur 115
+#define InitPosTibia 110
+
+// Motor Id Numbers
+#define TotalMotors 3
+//#define TotalMotors 18
+
+#define LFCoxaMotorId 1
+#define LFFemurMotorId 2
+#define LFTibiaMotorId 3
+
+#define RFCoxaMotorId 4
+#define RFFemurMotorId 5
+#define RFTibiaMotorId 6
+
+#define LMCoxaMotorId 7
+#define LMFemurMotorId 8
+#define LMTibiaMotorId 9
+
+#define RMCoxaMotorId 10
+#define RMFemurMotorId 11
+#define RMTibiaMotorId 12
+
+#define LRCoxaMotorId 13
+#define LRFemurMotorId 14
+#define LRTibiaMotorId 15
+
+#define RRCoxaMotorId 16
+#define RRFemurMotorId 17
+#define RRTibiaMotorId 18
+
+// Min/Max Angles
+#define CoxaMin
+#define CoxaMax
+
+#define FemurMin
+#define FemurMax
+
+#define TibiaMin
+#define TibiaMax
+
+// Leg Length
+#define CoxaLength 58
+#define FemurLength 70
+#define TibiaLength 135
+
+// Leg Offset
