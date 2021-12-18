25f2bc72b80e6b1f80d1ca6f9200729947cf8d9f
Yeryeong Kim <76420409+icarusicarus@users.noreply.github.com>
2021-12-18 11:42:09 +0900

    tmp commit

--- Hexapod/hexapod.ino
+++ Hexapod/hexapod.ino
@@ -312,32 +312,9 @@ void setup()
         Serial.print(dxl_present_position);
         Serial.println(" ");
 
-        // R1
-        dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 1, 30, RCoxaStep1, &dxl_error);
-
-        if (dxl_comm_result != COMM_SUCCESS)
-        {
-            Serial.print(packetHandler->getTxRxResult(dxl_comm_result));
-        }
-        else if (dxl_error != 0)
-        {
-            Serial.print(packetHandler->getRxPacketError(dxl_error));
-        }
-
+        delay(500);
         // L3
         dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 11, 30, LFemurStep3, &dxl_error);
-
-        if (dxl_comm_result != COMM_SUCCESS)
-        {
-            Serial.print(packetHandler->getTxRxResult(dxl_comm_result));
-        }
-        else if (dxl_error != 0)
-        {
-            Serial.print(packetHandler->getRxPacketError(dxl_error));
-        }
-
-        // R1
-        dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 2, 30, RFemurStep1, &dxl_error);
 
         if (dxl_comm_result != COMM_SUCCESS)
         {
@@ -361,7 +338,18 @@ void setup()
         }
 
         // R1
-        dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 3, 30, RTibiaStep1, &dxl_error);
+        dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 1, 30, RCoxaStep1, &dxl_error);
+
+        if (dxl_comm_result != COMM_SUCCESS)
+        {
+            Serial.print(packetHandler->getTxRxResult(dxl_comm_result));
+        }
+        else if (dxl_error != 0)
+        {
+            Serial.print(packetHandler->getRxPacketError(dxl_error));
+        }
+        // R1
+        dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 2, 30, RFemurStep1, &dxl_error);
 
         if (dxl_comm_result != COMM_SUCCESS)
         {
@@ -372,7 +360,18 @@ void setup()
             Serial.print(packetHandler->getRxPacketError(dxl_error));
         }
 
-        delay(500);
+        // R1
+        dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 3, 30, RTibiaStep1, &dxl_error);
+
+        if (dxl_comm_result != COMM_SUCCESS)
+        {
+            Serial.print(packetHandler->getTxRxResult(dxl_comm_result));
+        }
+        else if (dxl_error != 0)
+        {
+            Serial.print(packetHandler->getRxPacketError(dxl_error));
+        }
+        delay(100);
         // R2
         dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, 1, 30, RCoxaStep2, &dxl_error);
 
